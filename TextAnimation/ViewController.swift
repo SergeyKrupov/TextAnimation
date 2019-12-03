f44f20b1b64e640346c090958aa9ac5579c7028c
Sergey V. Krupov <sergey.krupov@gmail.com>
2019-12-03 19:55:39 +0500

    Initial Commit

---
+++ TextAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextAnimation
+//
+//  Created by Sergey V. Krupov on 03.12.2019.
+//  Copyright © 2019 Sergey V. Krupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
