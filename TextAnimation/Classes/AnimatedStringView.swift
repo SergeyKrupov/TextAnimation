03d6c2c11a91f1174105d3942f792eb6423650a1
Sergey V. Krupov <sergey.krupov@gmail.com>
2019-12-04 20:50:55 +0500

    Preview

---
+++ TextAnimation/Classes/AnimatedStringView.swift
@@ -0,0 +1,31 @@
+//
+//  AnimatedStringView.swift
+//  TextAnimation
+//
+//  Created by Sergey V. Krupov on 04.12.2019.
+//  Copyright © 2019 Sergey V. Krupov. All rights reserved.
+//
+
+import UIKit
+
+final class AnimatedStringView: UIView {
+
+    var text: String? {
+        get {
+            stringLayer.text
+        }
+        set {
+            stringLayer.text = newValue
+            bounds.size = stringLayer.bounds.size
+        }
+    }
+
+    override class var layerClass: AnyClass {
+        return AnimatedStringLayer.self
+    }
+
+    // MARK: - Private
+    private var stringLayer: AnimatedStringLayer {
+        layer as! AnimatedStringLayer
+    }
+}
---
+++ TextAnimation/PreviewViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PreviewViewController.swift
+//  TextAnimation
+//
+//  Created by Sergey V. Krupov on 04.12.2019.
+//  Copyright © 2019 Sergey V. Krupov. All rights reserved.
+//
+
+import UIKit
+
+final class PreviewViewController: UIViewController {
+
+    @IBOutlet private var animatedStringView: AnimatedStringView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        animatedStringView.text = "Foo-Bar"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
